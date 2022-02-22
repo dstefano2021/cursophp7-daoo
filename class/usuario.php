065e1423869080161931d0f412030ab0b84eb146
dstefano2021 <du_stefano@yahoo.com.br>
2022-02-21 22:38:44 -0300

    Comando delete

--- class/usuario.php
+++ class/usuario.php
@@ -157,6 +157,19 @@ class Usuario {
         ));
     }
 
+    public function delete(){
+
+        $sql = new Sql();
+        $sql->query("DELETE FROM tb_usuarios WHERE idusuario = :ID", array(
+            ':ID'=>$this->getIdusuario()
+        ));
+
+            $this->setIdusuario(0);
+            $this->setDeslogin(0);
+            $this->setDessenha(0);
+            $this->setDtcadastro(new DateTime());
+    }
+
     public function __construct($login = "", $password = ""){
 
         $this->setDeslogin($login);
